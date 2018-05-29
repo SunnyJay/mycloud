2bc67e77d271ba676eb35cebba143962f91315dc
老抠AND小抠 <sun_coke007@163.com>
2018-05-29 22:27:21 +0800

    添加模板生成工具

---
+++ tangyuan/util/template_util.py
@@ -0,0 +1,59 @@
+import jinja2
+import sys
+import codecs
+import random
+import os
+
+project_name = "tangyuan"
+
+
+def get_template(searchpath, template_name):
+    templateLoader = jinja2.FileSystemLoader(searchpath=searchpath)
+    templateEnv = jinja2.Environment(loader=templateLoader)
+    template = templateEnv.get_template(template_name)
+    return template
+
+
+def render_template(app_list):
+    for app_name in app_list:
+        app_port = random.randint(8800, 9000)
+        render_dockerfile(app_name, app_port)
+        render_k8s_yaml(app_name, app_port)
+        render_ingress_yaml(app_name, app_port)
+
+
+def render_dockerfile(app_name, app_port):
+    template = get_template("templates", "Dockerfile")
+    render = template.render(project_name=project_name,
+                             app_name=app_name,
+                             app_port=app_port)
+    output_file(render, "Dockerfile", app_name)
+
+
+def output_file(render, dest, app_name):
+    if not os.path.exists(os.path.join("output", app_name)):
+        os.makedirs(os.path.join("output", app_name))
+    dest = os.path.join("output", app_name, dest)
+    with codecs.open(dest, 'wb', encoding='utf-8') as fh:
+        fh.write(render)
+
+
+def render_k8s_yaml(app_name, app_port):
+    template = get_template("templates", "k8s.yaml")
+    render = template.render(project_name=project_name,
+                             app_name=app_name,
+                             app_port=app_port)
+    output_file(render, "k8s.yaml", app_name)
+
+
+def render_ingress_yaml(app_name, app_port):
+    template = get_template("templates", "ingress.yaml")
+    render = template.render(project_name=project_name,
+                             app_name=app_name,
+                             app_port=app_port)
+    output_file(render, "ingress.yaml", app_name)
+
+
+if __name__ == '__main__':
+    render_template(["user", "monitor"])
+    # render_template(sys.argv)
