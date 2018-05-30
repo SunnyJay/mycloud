783a04bca1b08292adf384ca32ea4af192a99f14
sunnyjay <1710081230@qq.com>
2018-05-30 09:38:47 +0800

    添加路径判断

--- tangyuan/util/tar_copy_util.py
+++ tangyuan/util/tar_copy_util.py
@@ -7,9 +7,9 @@ def copy_tar():
     for dir in dir_list:
         if dir.startswith("tangyuan-"):
             src = os.path.join("..", dir, "build", "distributions", dir + ".tar")
-            dest = os.path.join("output", dir, dir + ".tar")
+            dest = os.path.join("output", dir)
             if os.path.exists(src) and os.path.exists(dest):
-                shutil.copy(src, dest)
+                shutil.copy(src, os.path.join(dest, dir + ".tar"))
 
 if __name__ == '__main__':
     copy_tar()
